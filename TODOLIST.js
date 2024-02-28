a1f6a2ba8e8e2a84300fd17b1780699680e2faeb
Anilshanigarapu <151608646+Anil-nani@users.noreply.github.com>
2024-02-28 14:35:07 +0530

    Add files via upload

---
+++ TODOLIST.js
@@ -0,0 +1,47 @@
+
+
+
+const myinput = document.getElementById("input")
+const mybtn = document.getElementById("add")
+const mylist = document.getElementById("list")
+
+mybtn.addEventListener("click",myfunc);
+
+function myfunc(){
+    // mylist.innerHTML =myinput.value
+   if(myinput.value ===''){
+alert('Please Enter Your Task') 
+   }else{
+    let nlist =document.createElement("li")
+    nlist.innerHTML =myinput.value
+     mylist.prepend(nlist)
+       myinput.value =""
+     let del = document.createElement("button")
+       del.innerHTML ="del"
+       
+       nlist.appendChild(del)
+       del.addEventListener("click" , mydel);
+       function mydel(){
+            nlist.remove()
+       }
+
+
+       let edit =document.createElement("button")
+       edit.innerHTML ="edit"
+       nlist.appendChild(edit)
+       
+       edit.addEventListener("click", myedit);
+       function myedit(){
+        
+          
+         del.remove()
+         edit.remove()
+         nlist.remove()
+         myinput.value =nlist.innerText
+        
+       }
+
+   
+    }
+   }
+        
